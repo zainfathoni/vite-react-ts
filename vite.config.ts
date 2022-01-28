571cf9e4d47420e9f22e39c523fd229249e73d2e
Zain Fathoni <me@zainf.dev>
2022-01-29 07:32:41 +0800

    build: integrate vite-plugin-ejs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteEjsPlugin } from 'vite-plugin-ejs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    ViteEjsPlugin({
+      HTML_TITLE: 'React + TypeScript + Vite',
+    }),
+    react(),
+  ],
 })
